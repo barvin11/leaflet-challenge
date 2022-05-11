85a6761447182ab66acddf5e9a5fe02716676e9c
Vincent Baria <Vince>
2022-05-11 03:04:46 -0400

    Adding API and improving code

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,7 +4,7 @@ var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // Performing a GET request to the query URL/
 d3.json(url).then(function(data) {
 
-    // Once we get a response, send the data.features object to the createFeatures fucntion.
+    // Once we get a response, send the data.features object to the createFeatures function.
     createFeatures(data.features);
 });
 
@@ -56,8 +56,10 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
     //Create the base layers.
-    var gray = L.tileLayer("https://maps.omniscale.net/v2/private-setordji-abotsi-4b2aefdb/style.grayscale/{z}/{x}/{y}.png", {
-        attribution: "©️ <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> ©️ <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    var gray = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {  
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        accessToken: API_KEY,
+        id: "mapbox/streets-v11"
     });
 
     // Setting up the legend
